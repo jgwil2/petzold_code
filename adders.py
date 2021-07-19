3cb271efe274fbc343f1278e0fb792176b834e3c
John Willard <jgwil2@gmail.com>
2021-07-19 18:15:37 -0400

    add full adder

--- adders.py
+++ adders.py
@@ -1,5 +1,5 @@
 from base import LogicComponent
-from gates import And, Split, Xor
+from gates import And, Or, Split, Xor
 
 
 class HalfAdder(LogicComponent):
@@ -31,3 +31,35 @@ class HalfAdder(LogicComponent):
         self.splitter_b.output_b.connections.append(self.xor_gate.input_b)
         self.output_carry = self.and_gate.output
         self.output_sum = self.xor_gate.output
+
+
+class FullAdder(LogicComponent):
+    """
+    Adds 3 bits - a, b, and a carry bit from a possible previous op
+    on a less-significant column.
+
+    | Input A | Input B | Carry In | Sum Out | Carry Out |
+    | ------- | ------- | -------- | ------- | --------- |
+    | 0       | 0       | 0        | 0       | 0         |
+    | 0       | 0       | 1        | 1       | 0         |
+    | 0       | 1       | 0        | 1       | 0         |
+    | 0       | 1       | 1        | 0       | 1         |
+    | 1       | 0       | 0        | 1       | 0         |
+    | 1       | 0       | 1        | 0       | 1         |
+    | 1       | 1       | 0        | 0       | 1         |
+    | 1       | 1       | 1        | 1       | 1         |
+    """
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.adder_1 = HalfAdder(f"{name}#adder_1")
+        self.adder_2 = HalfAdder(f"{name}#adder_2")
+        self.or_gate = Or(f"{name}#or_gate")
+        self.input_a = self.adder_1.input_a
+        self.input_b = self.adder_1.input_b
+        self.input_carry = self.adder_2.input_a
+        self.adder_1.output_sum.connections.append(self.adder_2.input_b)
+        self.adder_1.output_carry.connections.append(self.or_gate.input_b)
+        self.adder_2.output_carry.connections.append(self.or_gate.input_a)
+        self.output_sum = self.adder_2.output_sum
+        self.output_carry = self.or_gate.output
--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 
 from base import Level
 from gates import And, Nand, Nor, Not, Or, Xor
-from adders import HalfAdder
+from adders import FullAdder, HalfAdder
 
 
 class TestLogicGates(unittest.TestCase):
@@ -17,64 +17,64 @@ class TestLogicGates(unittest.TestCase):
         self.assertEqual(test_inverter.output.val, Level.HI, "!0 = 1")
 
     def test_and(self):
-        test_and = And("test_and_gate")
-        test_and.input_a.val = Level.HI
-        test_and.input_b.val = Level.HI
-        self.assertEqual(test_and.output.val, Level.HI, "1 x 1 = 1")
-        test_and.input_a.val = Level.LO
-        self.assertEqual(test_and.output.val, Level.LO, "0 x 1 = 0")
-        test_and.input_b.val = Level.LO
-        self.assertEqual(test_and.output.val, Level.LO, "0 x 0 = 0")
-        test_and.input_a.val = Level.HI
-        self.assertEqual(test_and.output.val, Level.LO, "1 x 0 = 0")
+        test_and_gate = And("test_and_gate")
+        test_and_gate.input_a.val = Level.HI
+        test_and_gate.input_b.val = Level.HI
+        self.assertEqual(test_and_gate.output.val, Level.HI, "1 x 1 = 1")
+        test_and_gate.input_a.val = Level.LO
+        self.assertEqual(test_and_gate.output.val, Level.LO, "0 x 1 = 0")
+        test_and_gate.input_b.val = Level.LO
+        self.assertEqual(test_and_gate.output.val, Level.LO, "0 x 0 = 0")
+        test_and_gate.input_a.val = Level.HI
+        self.assertEqual(test_and_gate.output.val, Level.LO, "1 x 0 = 0")
 
     def test_or(self):
-        test_or = Or("test_or_gate")
-        test_or.input_a.val = Level.HI
-        test_or.input_b.val = Level.HI
-        self.assertEqual(test_or.output.val, Level.HI, "1 + 1 = 1")
-        test_or.input_a.val = Level.LO
-        self.assertEqual(test_or.output.val, Level.HI, "0 + 1 = 1")
-        test_or.input_b.val = Level.LO
-        self.assertEqual(test_or.output.val, Level.LO, "0 + 0 = 0")
-        test_or.input_a.val = Level.HI
-        self.assertEqual(test_or.output.val, Level.HI, "1 + 0 = 1")
+        test_or_gate = Or("test_or_gate")
+        test_or_gate.input_a.val = Level.HI
+        test_or_gate.input_b.val = Level.HI
+        self.assertEqual(test_or_gate.output.val, Level.HI, "1 + 1 = 1")
+        test_or_gate.input_a.val = Level.LO
+        self.assertEqual(test_or_gate.output.val, Level.HI, "0 + 1 = 1")
+        test_or_gate.input_b.val = Level.LO
+        self.assertEqual(test_or_gate.output.val, Level.LO, "0 + 0 = 0")
+        test_or_gate.input_a.val = Level.HI
+        self.assertEqual(test_or_gate.output.val, Level.HI, "1 + 0 = 1")
 
     def test_nand(self):
-        test_nand = Nand("test_nand_gate")
-        test_nand.input_a.val = Level.HI
-        test_nand.input_b.val = Level.HI
-        self.assertEqual(test_nand.output.val, Level.LO, "!(1 x 1) = 0")
-        test_nand.input_a.val = Level.LO
-        self.assertEqual(test_nand.output.val, Level.HI, "!(0 x 1) = 1")
-        test_nand.input_b.val = Level.LO
-        self.assertEqual(test_nand.output.val, Level.HI, "!(0 x 0) = 1")
-        test_nand.input_a.val = Level.HI
-        self.assertEqual(test_nand.output.val, Level.HI, "!(1 x 0) = 1")
+        test_nand_gate = Nand("test_nand_gate")
+        test_nand_gate.input_a.val = Level.HI
+        test_nand_gate.input_b.val = Level.HI
+        self.assertEqual(test_nand_gate.output.val, Level.LO, "!(1 x 1) = 0")
+        test_nand_gate.input_a.val = Level.LO
+        self.assertEqual(test_nand_gate.output.val, Level.HI, "!(0 x 1) = 1")
+        test_nand_gate.input_b.val = Level.LO
+        self.assertEqual(test_nand_gate.output.val, Level.HI, "!(0 x 0) = 1")
+        test_nand_gate.input_a.val = Level.HI
+        self.assertEqual(test_nand_gate.output.val, Level.HI, "!(1 x 0) = 1")
 
     def test_nor(self):
-        test_nor = Nor("test_nor_gate")
-        test_nor.input_a.val = Level.HI
-        test_nor.input_b.val = Level.HI
-        self.assertEqual(test_nor.output.val, Level.LO, "!(1 + 1) = 0")
-        test_nor.input_a.val = Level.LO
-        self.assertEqual(test_nor.output.val, Level.LO, "!(0 + 1) = 0")
-        test_nor.input_b.val = Level.LO
-        self.assertEqual(test_nor.output.val, Level.HI, "!(0 + 0) = 1")
-        test_nor.input_a.val = Level.HI
-        self.assertEqual(test_nor.output.val, Level.LO, "!(1 + 0) = 0")
+        test_nor_gate = Nor("test_nor_gate")
+        test_nor_gate.input_a.val = Level.HI
+        test_nor_gate.input_b.val = Level.HI
+        self.assertEqual(test_nor_gate.output.val, Level.LO, "!(1 + 1) = 0")
+        test_nor_gate.input_a.val = Level.LO
+        self.assertEqual(test_nor_gate.output.val, Level.LO, "!(0 + 1) = 0")
+        test_nor_gate.input_b.val = Level.LO
+        self.assertEqual(test_nor_gate.output.val, Level.HI, "!(0 + 0) = 1")
+        test_nor_gate.input_a.val = Level.HI
+        self.assertEqual(test_nor_gate.output.val, Level.LO, "!(1 + 0) = 0")
 
     def test_xor(self):
-        test_xor = Xor("test_xor_gate")
-        test_xor.input_a.val = Level.HI
-        test_xor.input_b.val = Level.HI
-        self.assertEqual(test_xor.output.val, Level.LO, "(1 + 1) * (!1 + !1) = 0")
-        test_xor.input_a.val = Level.LO
-        self.assertEqual(test_xor.output.val, Level.HI, "(0 + 1) * (!0 + !1) = 1")
-        test_xor.input_b.val = Level.LO
-        self.assertEqual(test_xor.output.val, Level.LO, "(0 + 0) * (!0 + !0) = 0")
-        test_xor.input_a.val = Level.HI
-        self.assertEqual(test_xor.output.val, Level.HI, "(1 + 0) * (!1 + !0) = 1")
+        test_xor_gate = Xor("test_xor_gate")
+        test_xor_gate.input_a.val = Level.HI
+        test_xor_gate.input_b.val = Level.HI
+        self.assertEqual(test_xor_gate.output.val, Level.LO, "(1 + 1) * (!1 + !1) = 0")
+        test_xor_gate.input_a.val = Level.LO
+        self.assertEqual(test_xor_gate.output.val, Level.HI, "(0 + 1) * (!0 + !1) = 1")
+        test_xor_gate.input_b.val = Level.LO
+        self.assertEqual(test_xor_gate.output.val, Level.LO, "(0 + 0) * (!0 + !0) = 0")
+        test_xor_gate.input_a.val = Level.HI
+        self.assertEqual(test_xor_gate.output.val, Level.HI, "(1 + 0) * (!1 + !0) = 1")
 
 
 class TestAdders(unittest.TestCase):
@@ -93,6 +93,31 @@ class TestAdders(unittest.TestCase):
         half_adder.input_b.val = Level.LO
         self.assertEqual(half_adder.output_sum.val, Level.LO, "0 + 0: sum bit is 0")
         self.assertEqual(half_adder.output_carry.val, Level.LO, "0 + 0: carry bit is 0")
+
+    def test_full_adder(self):
+        full_adder = FullAdder("test_full_adder")
+        full_adder.input_a.val = Level.HI
+        full_adder.input_b.val = Level.HI
+        full_adder.input_carry.val = Level.HI
+        self.assertEqual(full_adder.output_sum.val, Level.HI, "adder(1, 1, 1) = (1, 1)")
+        self.assertEqual(
+            full_adder.output_carry.val, Level.HI, "adder(1, 1, 1) = (1, 1)"
+        )
+        full_adder.input_carry.val = Level.LO
+        self.assertEqual(full_adder.output_sum.val, Level.LO, "adder(1, 1, 0) = (0, 1")
+        self.assertEqual(
+            full_adder.output_carry.val, Level.HI, "adder(1, 1, 0) = (0, 1)"
+        )
+        full_adder.input_b.val = Level.LO
+        self.assertEqual(full_adder.output_sum.val, Level.HI, "adder(1, 0, 0) = (1, 0")
+        self.assertEqual(
+            full_adder.output_carry.val, Level.LO, "adder(1, 0, 0) = (1, 0)"
+        )
+        full_adder.input_a.val = Level.LO
+        self.assertEqual(full_adder.output_sum.val, Level.LO, "adder(0, 0, 0) = (0, 0")
+        self.assertEqual(
+            full_adder.output_carry.val, Level.LO, "adder(0, 0, 0) = (0, 0)"
+        )
 
 
 if __name__ == "__main__":
