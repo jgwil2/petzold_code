0671037bdef005af2e1063cab12b640ef7767c55
John Willard <jgwil2@gmail.com>
2021-07-18 22:45:20 -0400

    add documentation

--- base.py
+++ base.py
@@ -3,11 +3,22 @@ from typing import List
 
 
 class Level(Enum):
+    """
+    Enumeration type for the value of a signal.
+    """
+
     LO = 0
     HI = 1
 
 
 class LogicComponent(object):
+    """
+    A gate or other chip. Is instantiated with a `name` for debugging.
+    Most components are built up from other components; some logic gates
+    are implemented as base components, meaning their logic is written
+    directly in Python rather than being composed of other components.
+    """
+
     def __init__(self, name):
         self.name = name
 
@@ -21,12 +32,18 @@ class LogicComponent(object):
         """
         Must be implemented for base components - complex chips are
         just compositions of base components so they will automatically
-        emit update their output signals when input signals change
+        update their output signals when input signals change.
         """
         pass
 
 
 class Pin(object):
+    """
+    Base class for a inputs and outputs. A Pin tracks its LogicComponent
+    so that it can invoke the `evaluate` method (in the case of an
+    InputPin) and for debugging. The Pin's `val` can be `HI` or `LO`.
+    """
+
     def __init__(self, component: LogicComponent):
         self.component: LogicComponent = component
 
@@ -34,6 +51,11 @@ class Pin(object):
 
 
 class InputPin(Pin):
+    """
+    An InputPin provides a value to a LogicComponent and forces that
+    component to evaluate (i.e. update its output based on its input).
+    """
+
     @property
     def val(self):
         return self._val
@@ -45,6 +67,13 @@ class InputPin(Pin):
 
 
 class OutputPin(Pin):
+    """
+    An OutputPin is assigned a value by a LogicComponent. It can be
+    wired to one or more InputPins, whose value it updates whenever its
+    own value is updated. These pins are tracked in the `connections`
+    list.
+    """
+
     def __init__(self, component):
         super().__init__(component)
         self.connections: List[InputPin] = []
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from gates import Not, Level, And
+from gates import And, Level, Nand, Not
 
 
 def main():
@@ -12,10 +12,16 @@ def main():
     print(f"inverter_a output = {sample_inverter_a.output.val}")
     print(f"inverter_b output = {sample_inverter_b.output.val}")
 
-    sample_and_gate = And("and")
-    sample_and_gate.input_a.val = Level.HI
-    sample_and_gate.input_b.val = Level.HI
-    print(f"and output = {sample_and_gate.output.val}")
+    sample_nand_gate = Nand("nand")
+    sample_inverter_c = Not("c")
+    sample_nand_gate.output.connections.append(sample_inverter_c.input)
+    sample_nand_gate.input_a.val = Level.HI
+    sample_nand_gate.input_b.val = Level.HI
+    print(f"nand output = {sample_nand_gate.output.val}")
+    print(f"inverter output = {sample_inverter_c.output.val}")
+    sample_nand_gate.input_b.val = Level.LO
+    print(f"nand output = {sample_nand_gate.output.val}")
+    print(f"inverter output = {sample_inverter_c.output.val}")
 
 
 if __name__ == "__main__":
