0926a22b38aa1347f94b5f256bb8681d1a108f39
John Willard <jgwil2@gmail.com>
2022-03-26 16:10:44 -0400

    add tests for input output mixin

--- mixins.py
+++ mixins.py
@@ -1,12 +1,12 @@
 class EightBitInputOutputMixin(object):
-    def set_as_number(self, input: int, prefix: str):
+    def set_data_as_int(self, input: int, prefix: str):
         # convert decimal to binary str and
         # iterate least-significant to most-significant
         input_str = format(input, "08b")
         for i in range(8):
             getattr(self, f"{prefix}_{7-i}").val = 1 if input_str[i] == "1" else 0
 
-    def get_as_number(self, prefix: str) -> int:
+    def get_data_as_int(self, prefix: str) -> int:
         bin_str = ""
         for i in range(8):
             bin_str += "1" if getattr(self, f"{prefix}_{7-i}").val == 1 else "0"
---
+++ tests/test_mixins.py
@@ -0,0 +1,31 @@
+import unittest
+
+from adders import EightBitAdder
+from latches import EightBitLatch
+
+
+class TestMixins(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_adder_with_mixin(self):
+        test_adder = EightBitAdder("test_adder")
+        test_adder.set_data_as_int(1, "input_a")
+        test_adder.set_data_as_int(1, "input_b")
+        self.assertEqual(test_adder.get_data_as_int("output_s"), 2)
+        test_adder.set_data_as_int(74, "input_b")
+        self.assertEqual(test_adder.get_data_as_int("output_s"), 75)
+        test_adder.set_data_as_int(39, "input_a")
+        self.assertEqual(test_adder.get_data_as_int("output_s"), 113)
+
+    def test_latch_with_mixin(self):
+        test_latch = EightBitLatch("test_latch")
+        test_latch.set_data_as_int(32, "d")
+        self.assertEqual(test_latch.get_data_as_int("q"), 0)
+        test_latch.clock.val = 1
+        self.assertEqual(test_latch.get_data_as_int("q"), 32)
+        test_latch.clock.val = 0
+        test_latch.set_data_as_int(132, "d")
+        self.assertEqual(test_latch.get_data_as_int("q"), 32)
+        test_latch.clock.val = 1
+        self.assertEqual(test_latch.get_data_as_int("q"), 132)
